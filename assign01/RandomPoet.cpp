13fa653fb0a80d86fe6ca1ca24975cdcae30f659
Charley Burtwistle <charley.burtwistle@doane.edu>
2017-09-05 23:39:57 +0000

    succesfully opens dictionary.txt

--- assign01/RandomPoet.cpp
+++ assign01/RandomPoet.cpp
@@ -2,12 +2,17 @@
 #include <cstdlib>
 #include <iostream>
 #include <stdexcept>
+#include <fstream>
 
 int main() {
 
   using namespace std;
 
   SimpleSLL list;
+  
+  //open input file
+  ifstream inFile;
+  inFile.open("dictionary.txt");
 
 
     list.add("HelloWorld");
@@ -15,7 +20,7 @@ int main() {
 
   cout << "Initial list has " << list.size() << " nodes:" << endl;
   list.print();
-  
+  inFile.close();
   
   return EXIT_SUCCESS;
 }
