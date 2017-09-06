2ab79fd5cc82481ea8dec91d7097c54109e2c85b
Charley Burtwistle <charley.burtwistle@doane.edu>
2017-09-06 00:52:49 +0000

    adds error checking for entering an int

--- assign01/RandomPoet.cpp
+++ assign01/RandomPoet.cpp
@@ -10,6 +10,9 @@ int main() {
 
   SimpleSLL list;
   
+  cout <<"Welcome to the random poetry generator!" <<
+         "Please wait while the dictiionary is loaded." << endl;
+  
   //open input file
   ifstream inFile;
   inFile.open("dictionary.txt");
@@ -19,6 +22,37 @@ int main() {
     list.add(all);
   }
   
+  
+  int again = 1;
+  int lines;
+  cout << "Let's make some poetry, dude! Enter the number of lines:";
+  cin >> lines;
+  while (cin.fail()) {
+    cout << "Please Enter an int." << std::endl;
+    cin.clear();
+    cin.ignore(256,'\n');
+    cin >> lines;
+}
+    
+
+  
+  int words;
+  cout << "Radical! Now, enter the number of words per line: ";
+  cin >> words;
+  while (cin.fail()) {
+  cout << "Please Enter an int." << std::endl;
+  cin.clear();
+  cin.ignore(256,'\n');
+  cin >> lines;
+    }
+  
+
+  
+
+  cout << "Would you like to make another poem? Enter 1 for yes, 0 for no: ";
+  cin >> again;
+ 
+
 
 
 
