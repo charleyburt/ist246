9bc66b6b4c388841b2b9ae39bf167e4d51671b80
Charley Burtwistle <charley.burtwistle@doane.edu>
2017-09-13 03:35:57 +0000

    removes old comments

--- assign02/TestSLL.cpp
+++ assign02/TestSLL.cpp
@@ -28,28 +28,5 @@ int main() {
 
 
 
-    // list.add(5);
-    // cout << list << endl;
-    // list.add(4);
-    // cout << list << endl;
-    // list.add(8);
-    // cout << list << endl;
-    // list.add(0);
-    // cout << list << endl;
-    // list.add(7);
-    // cout << list << endl;
-    // list.add(6);
-    // cout << list << endl;
-    // list.add(3);
-    // cout << list << endl;
-    // list.add(1);
-    // cout << list << endl;
-    // list.add(2);
-    // cout << list << endl;
-    // list.add(9);
-    // cout << list << endl;
-
-
-
   return EXIT_SUCCESS;
 }
