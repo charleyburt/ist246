a37891042dcb2f8d2ec098ce3da20269204e8afa
Charley Burtwistle <charley.burtwistle@doane.edu>
2017-09-13 03:32:20 +0000

    final commit, works as expected

--- assign02/SortedSLL.h
+++ assign02/SortedSLL.h
@@ -205,34 +205,64 @@ template <class T> void SLL<T>::add(const T &d) {
   //create a new Node
   Node *pN = new Node(d, pHead);
   
+  //If the list is empty
+  if (pHead == 0){
+    //set the head to the new node
+    pHead = pN;
+    
+    // if the list is not empty
+  }else{
+    
+  // create cursors
+  Node *pCurr = pHead, *pPrev = 0;
+    
+  // iterate through the list
+  while(pCurr != 0){
+    
+    //if this is the location where node should be inserted
+    if(pCurr->data >= pN->data){
+      
+      //break out of while loop
+      break;
+      
+      //else this is not the location
+    }else{
+      
+      // "inchworm" up to next node
+      pPrev = pCurr;
+      pCurr = pCurr->pNext;
+    }
+    //and try again to see if this is the location
+  }//end while
   
-  //if this is the only element in the list
-  if(pHead == 0){
-
-    // change head to point to new node
+  
+  // If where it needs to be inserted is at the head
+  if(pCurr == pHead){
+    
+    //set the new node to point to the old head
+    pN->pNext = pHead;
+    
+    //set the new node as the new head
     pHead = pN;
   }
   
-  //if this will go before the first element
-    else if (pHead->data > pN->data) {
-
-      // insert the new node before the current head
-      pN->pNext = pHead;
-      //update the head to the new node
-      pHead = pN;
-    }
+  //else its going to be somewhere in the middle
+  //or, it didn't find anywhere in the middle, and will add it to the end (i.e. pCurr == 0)
+  else{
     
-    else{
-    std::cout << "insert in the middle or end" << std::endl;      
-    }
+    //set the new node to point to the current node
+    pN->pNext = pCurr;
     
-    
-    
-
+    //set the previous node to point to the new node
+    pPrev->pNext = pN;
+  }
+  }
+  
+  //increment size by one
+  n++;
 
   
-  
-}
+}//end add
 
 /*
  * Delete all list nodes.
--- assign02/TestSLL.cpp
+++ assign02/TestSLL.cpp
@@ -8,28 +8,46 @@ int main() {
   using namespace std;
 
   SLL<int> list;
+  
+  int testArray [10] = {5,4,8,0,7,6,3,1,2,9};
+  
+  cout << "Here are the unsorted numbers: \n";
+  for (int unsortedNumbers : testArray){
+    cout << unsortedNumbers << " ";
+
+  }
+  
+  cout << endl;
+  
+  cout << "\nHere we add each of the unsorted numbers to a list, which will be sorted (hopefully): \n";
+  for (int unsortedNumbers : testArray){
+    cout << "add " << unsortedNumbers << ": ";
+    list.add(unsortedNumbers);
+    cout << list << endl;
+  }
 
 
-    list.add(5);
-    cout << list << endl;
-    list.add(4);
-    cout << list << endl;
-    list.add(8);
-    cout << list << endl;
-    list.add(0);
-    cout << list << endl;
-    list.add(7);
-    cout << list << endl;
-    list.add(6);
-    cout << list << endl;
-    list.add(3);
-    cout << list << endl;
-    list.add(1);
-    cout << list << endl;
-    list.add(2);
-    cout << list << endl;
-    list.add(9);
-    cout << list << endl;
+
+    // list.add(5);
+    // cout << list << endl;
+    // list.add(4);
+    // cout << list << endl;
+    // list.add(8);
+    // cout << list << endl;
+    // list.add(0);
+    // cout << list << endl;
+    // list.add(7);
+    // cout << list << endl;
+    // list.add(6);
+    // cout << list << endl;
+    // list.add(3);
+    // cout << list << endl;
+    // list.add(1);
+    // cout << list << endl;
+    // list.add(2);
+    // cout << list << endl;
+    // list.add(9);
+    // cout << list << endl;
 
 
 
